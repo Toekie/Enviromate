9d65bc018b3933e4be389f70fa48b5e27f7b62a2
Toekie <46795934+Toekie@users.noreply.github.com>
2021-02-05 14:42:00 +0100

    Added auto summer- and wintertime switching, Use with enviromate_43.tft nextion file.
    Added automatic switching between summertime and winter during march and oktober beside the manual option on the system page. This will also work as an extra control to the manual function as it will check the date whenever the system resets to put it on the right time.

---
+++ Enviromate_48/IO.ino
@@ -0,0 +1,324 @@
+/* omdat de 3 led verlichting verschillende PWM waarden kunnen hebben is de tijdspanne tussen elke stap
+verhoging of verlaging verschillend. Ele led verlichting heeft daarom zijn eigen interval.
+Hierdoor zullen de PWM waarden in alle gevallen even veel tijd nodig hebben om van 0 naar max te gaan.
+*/
+
+void setPWMinterval(byte dev){
+  word tv = tijdcode + tijdverloop;
+  /*interval tussen zon-op + tijdsverloop en zon-onder */
+  if ( (tv > timers[0][1]) && (tijdcode < timers[0][2]) ) {
+    interval[dev] = 50;
+  }
+
+  /*interval indien PWM gewijzigd na zon-onder */
+  else if ( (tijdcode > timers[0][2]) && (maxPWMval[dev] < PWMval[dev]) ) {
+    interval[dev] = 50;
+  }
+
+  /*interval gedurende resterende fade time */
+  else
+  {
+    if (fadetime >0){
+      interval[dev] = fadetime * 1000 / (PWMval[dev]+1);
+    }
+  }
+
+//  if (interval[dev] == 0) {interval[dev] = 1000;} 
+}
+
+void intervalcheck() { 
+  if (millis() > PWM_timer[0]) {
+    setPWMinterval(0);
+    PWM_timer[0] = millis() + interval[0]; // instellen volgende interval timer   
+    regelPWMA(0);
+  }
+  if (millis() > PWM_timer[1]) {
+    setPWMinterval(1);
+    PWM_timer[1] = millis() + interval[1];
+    regelPWMA(1);
+  }
+  if (millis() > PWM_timer[2]) {
+    setPWMinterval(2);
+    PWM_timer[2] = millis() + interval[2];
+    regelPWMA(2);
+  }
+}
+
+/* iedere intervalstap zal de PWM waarde verhogen of verlagen. */
+void regelPWMA(byte n) {
+  // verhoog of verlaag PWM waarde van 0 naar maxPWMval in 30 minuten.
+  if (timers[0][0] == 1) {
+    if (PWMval[n] < maxPWMval[n]) {
+      PWMval[n]++; // ophogen actuele PWM waarde
+    }
+    else if (PWMval[n] > maxPWMval[n]) {
+      PWMval[n] --;
+    }
+  }
+  else {
+    if (PWMval[n] > 0)  {
+      PWMval[n] --; // verlagen actuele PWM waarde
+    }
+  }
+
+ /* PWM waarde indien knoppen pageLight = "On" */
+  byte p_val[3];
+  p_val[0] = (LedOnOff[0] == 1) ? PWMval[0] : 0;
+  p_val[1] = (LedOnOff[1] == 1) ? PWMval[1] : 0;
+  p_val[2] = (LedOnOff[2] == 1) ? PWMval[2] : 0; 
+  // aansturen IO pinnen 
+  analogWrite(LEDpinA, p_val[0]);
+  analogWrite(LEDpinB, p_val[1]);
+  analogWrite(LEDpinC, p_val[2]);
+}
+
+
+/* aansturen IO pin foggers*/
+void fogger() {
+  byte foggerIO = 1;
+  
+  /* fogger = on indien timer == 1 en pagebutton == 1 */
+  if ((timers[3][0] == 1) && (OnOff[3] == 1)) {
+    foggerIO = 0;
+  }
+  if ((timers[4][0] == 1) && (OnOff[4] == 1)) {
+    foggerIO = 0;
+  }
+  if ((timers[5][0] == 1) && (OnOff[5] == 1)) {
+    foggerIO = 0;
+  }
+
+  digitalWrite(pinFogger, foggerIO);
+}
+
+
+void fanPWM() {
+  byte fpwm;
+  fpwm = (FanOnOff[0] == 1) ? FanPWM[0] : 0;
+  analogWrite(pinFanA, 255-fpwm);
+
+  fpwm = (FanOnOff[1] == 1) ? FanPWM[1] : 0;
+  analogWrite(pinFanB, 255-fpwm);
+
+  fpwm = (FanOnOff[2] == 1) ? FanPWM[2] : 0;
+  analogWrite(pinFanC, 255-fpwm);
+}
+
+
+void UV_OnOff(){
+  byte aan = LOW;
+  byte uit = HIGH;
+
+  //UV lamp gekoppeld aan zon-op/onder
+  if (OnOff[1] == 1){
+    if (PWMval[0] >= maxPWMval[0]) {
+      UVlamp = 1;
+      digitalWrite(UV_lamp, aan);
+    }
+    else {
+      UVlamp = 0;
+      digitalWrite(UV_lamp, uit);
+    } 
+  }
+  // UV lamp op eigen timer 
+  else { /* OnOff[1] == 0 */
+    if (timers[1][0] == 1){
+      UVlamp = 1;
+      digitalWrite(UV_lamp, aan);
+    }
+    else {
+      UVlamp = 0;
+      digitalWrite(UV_lamp, uit);
+    }    
+  }
+}
+
+
+void Warmte_OnOff(){
+  byte aan = LOW;
+  byte uit = HIGH;
+
+  //warmte lamp gekoppeld aan zon-op/onder
+  if (OnOff[2] == 1){
+    if (PWMval[0] >= maxPWMval[0]) {
+      infrarood = 1;
+      digitalWrite(heat_lamp, aan);
+    }
+    else {
+      infrarood = 0;
+      digitalWrite(heat_lamp, uit);
+    } 
+  }
+  // Warmte lamp op eigen timer 
+  else {
+    if (timers[2][0] == 1){
+      infrarood = 1;
+      digitalWrite(heat_lamp, aan);
+    }
+    else {
+      infrarood = 0;
+      digitalWrite(heat_lamp, uit);
+    }    
+  }
+}
+
+
+void relais() {
+  byte aanuit;
+  // extra relais aan timer
+  aanuit = (timers[6][0] == 1) ? LOW : HIGH; // OnOff
+  digitalWrite(pinRelaisA, aanuit);
+  aanuit = (timers[7][0] == 1) ? LOW : HIGH; // OnOff
+  digitalWrite(pinRelaisB, aanuit);
+  aanuit = (timers[8][0] == 1) ? LOW : HIGH; // OnOff
+  digitalWrite(pinRelaisC, aanuit);
+  aanuit = (timers[9][0] == 1) ? LOW : HIGH; // OnOff
+  digitalWrite(pinRelaisD, aanuit);
+}
+
+
+void spraytime() {
+ if ((sprayActief[1] > 0) || (spraytest)) {
+    digitalWrite(sprayPomp, LOW);
+    sprayActief[1]--;
+    if (sprayActief[1] == 0) {
+      spraytest = false;
+      Serial1.print("Mister.b0.txt=\"Test\"");
+      endString();
+      }
+    else {
+      digitalWrite(sprayPomp, HIGH);
+      Serial1.print("Mister.b0.txt=\""+String(sprayActief[1])+"\"");
+      endString();
+    }
+  }
+
+// blokkeerteller, voorkomt dat binnen 1 minuut de spray weer actief word
+  if (sprayActief[0] > 0) {
+    sprayActief[0]--;
+  }
+
+}
+
+void alarmBeep(){
+  if (DHTalarm + UVIalarm > 0){
+    beep = !beep;
+    }
+  else {
+    beep = 0;
+  }
+  digitalWrite(pinBeeper, beep);
+}
+
+
+//-------------------------------------Humidity and Temperature------------------------------------------------
+void readDHT(){
+  DHTcount++;
+  if (DHTcount < 10){return;} // 1x per 10 sec.
+
+  DHTcount = 0;
+  humidity = dht.getHumidity();     //Read  humidity , Reading humidity takes about 250 milliseconds!
+  temperature = dht.getTemperature(); //Read temperature, Reading temperature takes about 250 milliseconds!
+
+  if (temperature < mintemp){
+    DHTalarm = 1;
+  }
+  else if (temperature > maxtemp){
+    DHTalarm = 2;
+  }
+  else {DHTalarm = 0;}
+
+  if (Debug){
+ // Serial.print(dht.getStatusString());
+    Serial.print("humidity: ");
+    Serial.print(humidity, 1);
+    Serial.print(", temperature: ");
+    Serial.print(temperature, 1);
+    Serial.print(", max temp: ");
+    Serial.print(maxtemp);
+    Serial.print(", mintemp: ");
+    Serial.print(mintemp);
+    Serial.print(", DHTalarm: ");
+    Serial.print(DHTalarm);
+    Serial.print(", UVIalarm: ");
+    Serial.println(UVIalarm);
+  }
+}
+//-----------------------------------------------Day temperature control---------------------------------------
+
+
+//UVI begin --------------------------------------------------------------------------------------
+
+float UVI()
+{
+  int uvLevel = averageAnalogRead(UVOUT);
+  int refLevel = averageAnalogRead(REF_3V3);
+
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 3.3 / refLevel * uvLevel;
+
+  float uvIntensity = mapfloat(outputVoltage, UVIcal, 2.8, 0.0, 15.0); //Convert the voltage to a UV intensity level
+  uvIndex = ((outputVoltage - UVIcal) / 0.08) - 1; // uvIntensity converted to UV Index value
+  
+  //Serial.print(refLevel);
+  //Serial.print(uvLevel);
+  //Serial.print(outputVoltage);
+  //Serial.print(uvIntensity);
+  //Serial.print(uvIndex);
+
+  char uvIndexlcdresult[10]; // Buffer big enough for 9-character float
+  dtostrf(uvIndex, 8, 3, uvIndexlcdresult); // Leave room for too large numbers!
+
+//  Serial.print(uvIndexlcdresult);
+//  Serial.print(uvIndexlcdresult);
+
+  UVIalarm = 0;
+  if (uvIndex < minUVindx){ UVIalarm = 1;}
+  if (uvIndex > maxUVindx){ UVIalarm = 2;}
+   
+ return uvIndex;
+}
+
+
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0;
+
+  for (int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return (runningValue);
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+//UVI einde --------------------------------------------------------------------------------------
+
+
+void backlight(){
+  word LDRValue = analogRead(A7);
+  word valLDR;
+  if (LDRValue < LDRdrempel) { valLDR = map(LDRValue, 0, LDRdrempel, 30, 70);}
+  else { valLDR = map(LDRValue, LDRdrempel, 1024, 70, 100); }
+
+  if (valLDR > displayLight) {displayLight+=1;}
+  if (valLDR < displayLight) {displayLight-=1;}
+  if (displayLight > 100) {displayLight = 100;}
+  if (lastdisplayLight != displayLight){ // stuur dimwaarde indien veranderd
+    lastdisplayLight = displayLight;
+    Serial1.print("dim=");
+    Serial1.print(displayLight);
+    endString();
+/*
+    Serial.print("dim=");
+    Serial.print(displayLight);
+    Serial.print(", ");
+*/
+  }
+}
---
+++ Enviromate_48/setup.ino
@@ -0,0 +1,360 @@
+/* opslaan en inlezen settings in EEPROM */
+
+void storeZomertijd(){
+  // store autozomertijd in eeprom
+  EEPROM.write(mem_Zt,autozomertijd);
+}
+
+void readZomertijd(){
+  autozomertijd = EEPROM.read(mem_Zt); 
+}
+
+void uvicalibrate(String input){
+  UVIcal = input.toFloat();
+  UVIcal /= 1000;
+  EEPROM.write(mem_UVcal,   input[0]);
+  EEPROM.write(mem_UVcal+1, input[1]);
+  EEPROM.write(mem_UVcal+2, input[2]); 
+  Serial.println(UVIcal,3);
+}
+
+void readUVIcal(){
+   String val = "";
+   char data;
+   data = EEPROM.read(mem_UVcal);
+   val += data;
+   data = EEPROM.read(mem_UVcal+1);
+   val += data;
+   data = EEPROM.read(mem_UVcal+2);
+   val += data;
+   UVIcal = val.toFloat();
+   UVIcal /= 1000;
+}
+
+void storeMinuvIndex(String input){
+  word val = input.toInt();
+  byte hb = val / 100; // highbit
+  byte lb = val % 100; // lowbit
+  
+  EEPROM.write(mem_UVcal+3, hb);
+  EEPROM.write(mem_UVcal+4, lb);
+  minUVindx = input.toFloat();
+  minUVindx /= 100;  
+}
+
+void storeMaxuvIndex(String input){
+  word val = input.toInt();
+  byte hb = val / 100; // highbit
+  byte lb = val % 100; // lowbit
+  
+  EEPROM.write(mem_UVcal+5, hb);
+  EEPROM.write(mem_UVcal+6, lb);
+  maxUVindx = input.toFloat();
+  maxUVindx /= 100;  
+}
+
+void readMinMaxuvIndex(){
+  float val;
+  minUVindx = EEPROM.read(mem_UVcal+3);
+  val=        EEPROM.read(mem_UVcal+4);
+  val /= 100;
+  minUVindx+=val;
+
+  maxUVindx = EEPROM.read(mem_UVcal+5);
+  val=        EEPROM.read(mem_UVcal+6);
+  val /= 100;
+  maxUVindx+=val;
+
+  Serial.print("minUVindx: ");
+  Serial.print(minUVindx,2);
+  Serial.print(", maxUVindx: ");
+  Serial.println(maxUVindx,2);
+}
+
+
+void storeMaxtemp(String input){
+  word val = input.toInt();
+  byte hb = val / 10; // highbit
+  byte lb = val % 10; // lowbit
+  
+  EEPROM.write(mem_dht, hb);
+  EEPROM.write(mem_dht + 1, lb);
+  maxtemp = input.toFloat();
+  maxtemp /= 10;  
+}
+
+void storeMintemp(String input){
+  word val = input.toInt();
+  byte hb = val / 10; // highbit
+  byte lb = val % 10; // lowbit
+  
+  EEPROM.write(mem_dht + 2, hb);
+  EEPROM.write(mem_dht + 3, lb);
+  mintemp = input.toFloat();
+  mintemp /= 10;  
+}
+
+void readTemp(){
+  float val;
+  maxtemp = EEPROM.read(mem_dht);
+  val =     EEPROM.read(mem_dht+1);
+  val /= 10;
+  maxtemp+=val;
+  
+  mintemp =  EEPROM.read(mem_dht+2);
+  val=       EEPROM.read(mem_dht+3);
+  val /= 10;
+  mintemp+=val;
+  Serial.print("mintemp: ");
+  Serial.print(mintemp,1);    
+  Serial.print(", maxtemp: ");
+  Serial.println(maxtemp,1);
+}
+
+/* Spray time settings begin ====================*/
+void storeSprayTimers(byte nr){
+  byte adr = mem_spray;
+  byte H;
+  byte L;
+
+  for (int i = 0; i <= 2; i++) {
+    if (i == nr){
+      H = spray[i] / 100;
+      L = spray[i] % 100;
+      EEPROM.write(adr, H);
+      EEPROM.write(adr+1, L);
+    }
+    adr+=3;
+  }
+}
+
+void storeSprayDuration(byte nr){
+  byte adr = mem_spray;
+
+  for (int i = 0; i <= 2; i++) {
+    if (i == nr){
+     // Serial.print("saveSprayDuration ");
+     // Serial.println(i);
+      EEPROM.write(adr+2, sprayduration[i]);
+    }
+    adr+=3;
+  }
+}
+
+void readSprayTimers(){
+  byte adr = mem_spray;
+  byte val;
+  word tval;
+  Serial.println("spraytimers");
+  for (int i = 0; i <= 2; i++) {
+    // tijdstip aan
+    val = EEPROM.read(adr);
+    tval = val * 100; // H
+    adr++;
+    val = EEPROM.read(adr);
+    tval += val; // L
+    spray[i] = tval;
+    adr++;
+    sprayduration[i] = EEPROM.read(adr);
+    adr++;
+    Serial.print(i+1);
+    Serial.print(": ");
+    Serial.print(MinutesToStr(spray[i]));
+    Serial.print(" - ");
+    Serial.print(sprayduration[i]);
+    Serial.print(" - ");
+    Serial.println(sprayOnOff[i]);
+
+  }
+}
+/* Spray time and duration settings end ====================*/
+
+
+/* timer settings begin ====================================*/
+void storeTimers(){
+  byte adr = mem_timers;
+  byte h;
+  byte m;
+
+  for (int i = 0; i <= 9; i++) {
+    // tijstip aan
+    h = timers[i][1] / 100; // uur
+    m = timers[i][1] % 100; // min
+    EEPROM.write(adr + 0, h);
+    EEPROM.write(adr + 1, m);
+
+    // tijdstip uit
+    h = timers[i][2] / 100; // uur
+    m = timers[i][2] % 100; // min
+    EEPROM.write(adr + 2, h);
+    EEPROM.write(adr + 3, m);
+    adr += 4;
+  }
+}
+
+
+boolean readTimers() {
+  byte adr = mem_timers;
+  byte val;
+  word tval;
+  Serial.println("timers");
+  for (int i = 0; i <= 9; i++) {
+    // tijdstip aan
+    val = EEPROM.read(adr + 0);
+    tval = val * 100; // uur
+    val = EEPROM.read(adr + 1);
+    tval += val; // min
+    timers[i][1] = tval;
+
+    // tijdstip uit
+    val = EEPROM.read(adr + 2);
+    tval = val * 100; // uur
+    val = EEPROM.read(adr + 3);
+    tval += val; // min
+    timers[i][2] = tval;
+    adr += 4;
+    Serial.print(MinutesToStr(timers[i][1]));
+    Serial.print(" - ");
+    Serial.println(MinutesToStr(timers[i][2]));
+  }
+  return true;
+}
+/* timer settings end ====================================*/
+
+
+/* PWM settings begin ====================================*/
+// PWMval[10]
+// FanPWM[3]
+
+void storePWM(){
+  byte adr = mem_PWM;
+  byte i;
+  for ( i = 0; i <= 9; i++) {
+    EEPROM.write(adr + i, PWMval[i]);
+  }
+}
+
+void storeFanPWM(){
+  EEPROM.write(mem_PWM +10, FanPWM[0]);
+  EEPROM.write(mem_PWM +11, FanPWM[1]);
+  EEPROM.write(mem_PWM +12, FanPWM[2]);
+}
+
+void storeMaxPWM(){
+  EEPROM.write(mem_PWM +15, maxPWMval[0]);
+  EEPROM.write(mem_PWM +16, maxPWMval[1]);
+  EEPROM.write(mem_PWM +17, maxPWMval[2]); 
+}
+
+void readPWM(){
+  byte adr = mem_PWM;
+  byte i;
+  for ( i = 0; i <= 9; i++) {
+    PWMval[i] = EEPROM.read(adr + i);
+  }
+  FanPWM[0] = EEPROM.read(mem_PWM + 10);
+  FanPWM[1] = EEPROM.read(mem_PWM + 11);
+  FanPWM[2] = EEPROM.read(mem_PWM + 12);
+
+  maxPWMval[0] = EEPROM.read(mem_PWM + 15);
+  maxPWMval[1] = EEPROM.read(mem_PWM + 16);
+  maxPWMval[2] = EEPROM.read(mem_PWM + 17);
+
+}
+/* FanPWM settings end ===================================*/
+
+
+/* latitude/longitude settings begin======================*/
+void saveCoord(String coord) {
+  byte adr = mem_coord;
+  coord +="#";
+  //coord = latitude+";"+longitude+"#";
+  byte L = coord.length() + 1;
+/*
+  Serial.print("coordinates: ");
+  Serial.print(coord);
+  Serial.print(" L: ");
+  Serial.println(L);
+*/  
+  for (word i = 0; i <= L; i++)
+  {
+    EEPROM.write(mem_coord +i, coord[i]);
+    adr++;
+  }
+}
+
+
+void readCoord(){
+  byte adr = 0;
+  latitude = "";
+  longitude = "";
+  
+  for (byte i = 0; i <= 12; i++)
+  {
+    char data = EEPROM.read(mem_coord +i);
+    if (data != ';'){
+      latitude += data;
+    }
+    else {break;}
+   adr++;
+  }
+  
+  adr += mem_coord;
+  
+  for (byte i = 1; i <= 12; i++)
+  {
+    char data = EEPROM.read(adr +i);
+    if (data != '#'){
+      longitude += data;
+    }
+    else {break;}
+  }
+
+  Serial.println("latitude: "+latitude);
+  Serial.println("longitude: "+longitude);
+}
+/* latitude/longitude settings end  ======================*/
+
+
+void SetDefaultValues(){
+  /* omzetten tijd tekststrings naar minuten
+  en plaatsen in array timers[] */
+  convertTxt(0, LED_OnA);
+  convertTxt(1, UV);
+  convertTxt(2, Warmte);
+  convertTxt(3, tsw_3);
+  convertTxt(4, tsw_4);
+  convertTxt(5, tsw_5);
+  convertTxt(6, tsw_6);
+  convertTxt(7, tsw_7);
+  convertTxt(8, tsw_8);
+  convertTxt(9, tsw_9);   
+  // bewaar in EEPROM
+  for (int i = 0; i <= 2; i++) {
+    storeSprayTimers(i);
+    storeSprayDuration(i);
+  }
+  storeTimers();
+  storePWM();
+  storeMaxtemp("240");
+  storeMintemp("180");
+  saveCoord(latitude + ";" + longitude);
+  uvicalibrate("920");
+  storeMinuvIndex("2800");
+  storeMaxuvIndex("3000");
+  
+  storeCheck();
+  asm volatile ( "jmp 0"); // reboot arduino
+}
+
+boolean readCheck(){
+  if (EEPROM.read(0) != check) {
+    Serial.println("Store default val in eeprom");
+    return false;
+  }
+  else { return true;}
+}
+
+void storeCheck(){
+  EEPROM.write(0, check);
+}
